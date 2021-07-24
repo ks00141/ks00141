5176d614b518884e6f3ddd32e267d566076b4b6a
oruparu <ks00141@naver.com>
2021-07-25 08:01:17 +0900

    treeview

--- QT/prac1.py
+++ QT/prac1.py
@@ -1,1 +1,17 @@
-print('test')
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+import sys
+
+main_window = uic.loadUiType('main.ui')[0]
+
+
+class Main(QMainWindow, main_window):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.show()
+
+
+app = QApplication(sys.argv)
+w = Main()
+sys.exit(app.exec_())
